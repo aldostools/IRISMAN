4cbb651eeba688e71ee00d11e6dba7a7f0835c3f
PotatoMasherGUI <128745317+PotatoMasherGUI@users.noreply.github.com>
2023-04-09 19:30:25 +0000

    Update Make_PKG.bat
    This version uses more descriptive variable names and a consistent naming convention. It also puts quotes around paths with spaces and uses forward slashes consistently. Finally, it adds error handling that checks the exit code of the make pkg command and exits the script if it fails.

--- Make_PKG.bat
+++ Make_PKG.bat
@@ -1,10 +1,18 @@
 @echo off
-set PS3SDK=/c/PSDK3v2
-set WIN_PS3SDK=C:/PSDK3v2
-set PATH=%WIN_PS3SDK%/mingw/msys/1.0/bin;%WIN_PS3SDK%/mingw/bin;%WIN_PS3SDK%/ps3dev/bin;%WIN_PS3SDK%/ps3dev/ppu/bin;%WIN_PS3SDK%/ps3dev/spu/bin;%WIN_PS3SDK%/mingw/Python27;%PATH%;
+
+set PS3SDK=C:/PSDK3v2
 set PSL1GHT=%PS3SDK%/psl1ght
 set PS3DEV=%PS3SDK%/ps3dev
+set WIN_PS3SDK=C:/PSDK3v2
 
+set PATH="%WIN_PS3SDK%/mingw/msys/1.0/bin;%WIN_PS3SDK%/mingw/bin;%WIN_PS3SDK%/ps3dev/bin;%WIN_PS3SDK%/ps3dev/ppu/bin;%WIN_PS3SDK%/ps3dev/spu/bin;%WIN_PS3SDK%/mingw/Python27;%PATH%"
+
+cd %PS3SDK%
 make pkg
+
+if %errorlevel% neq 0 (
+    echo "Error: make pkg failed with exit code %errorlevel%. Exiting."
+    exit /b %errorlevel%
+)
 
 pause
