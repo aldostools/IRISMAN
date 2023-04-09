27a199137332ce5f5a5653ca25fb748b49b9e10d
PotatoMasherGUI <128745317+PotatoMasherGUI@users.noreply.github.com>
2023-04-09 20:18:12 +0000

    Update Make_PKG.bat
    .bat version (sorry for the mistake)

--- Make_PKG.bat
+++ Make_PKG.bat
@@ -1,18 +1,35 @@
 @echo off
+setlocal
 
-set PS3SDK=C:/PSDK3v2
-set PSL1GHT=%PS3SDK%/psl1ght
-set PS3DEV=%PS3SDK%/ps3dev
-set WIN_PS3SDK=C:/PSDK3v2
+set "PS3SDK=C:/PSDK3v2"
+set "PSL1GHT=%PS3SDK%/psl1ght"
+set "PS3DEV=%PS3SDK%/ps3dev"
+set "WIN_PS3SDK=C:/PSDK3v2"
 
-set PATH="%WIN_PS3SDK%/mingw/msys/1.0/bin;%WIN_PS3SDK%/mingw/bin;%WIN_PS3SDK%/ps3dev/bin;%WIN_PS3SDK%/ps3dev/ppu/bin;%WIN_PS3SDK%/ps3dev/spu/bin;%WIN_PS3SDK%/mingw/Python27;%PATH%"
+set "PATH=%WIN_PS3SDK%/mingw/msys/1.0/bin;%WIN_PS3SDK%/mingw/bin;%WIN_PS3SDK%/ps3dev/bin;%WIN_PS3SDK%/ps3dev/ppu/bin;%WIN_PS3SDK%/ps3dev/spu/bin;%WIN_PS3SDK%/mingw/Python27;%PATH%"
 
-cd %PS3SDK%
-make pkg
+if not exist "%PS3SDK%" (
+    echo PS3SDK is not set correctly. Please set it to the correct path.
+    exit /b 1
+)
 
-if %errorlevel% neq 0 (
-    echo "Error: make pkg failed with exit code %errorlevel%. Exiting."
-    exit /b %errorlevel%
+if not exist "%PS3DEV%" (
+    echo PS3DEV is not set correctly. Please set it to the correct path.
+    exit /b 1
+)
+
+if not exist "%PSL1GHT%" (
+    echo PSL1GHT is not set correctly. Please set it to the correct path.
+    exit /b 1
+)
+
+cd /d "%PS3SDK%" && (
+    make pkg
+    if %errorlevel% neq 0 (
+        echo.
+        echo Error: make pkg failed with exit code %errorlevel%. Exiting.
+        exit /b %errorlevel%
+    )
 )
 
 pause
